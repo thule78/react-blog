11fb6981c9373f94c949f201b79012a043ad9a56
thule <lunthu@gmail.com>
2019-06-12 16:42:36 +0700

    fix json placeholder

--- src/apis/jsonPlaceholder.jsx
+++ src/apis/jsonPlaceholder.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://jsonplaceholder.tyicode.com/'
+  baseURL: 'https://jsonplaceholder.typicode.com/'
 })
---
+++ src/reducers/postsReducer.jsx
@@ -0,0 +1,12 @@
+export default () => {
+  // bad !
+  // return document.querySelector('input')
+
+  // bad !
+  // return axios.get('/posts')
+
+  // good
+  // return state + action
+
+  return {};
+};
